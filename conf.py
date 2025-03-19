693decc9eb116b28626965d60f7f333ff488943f
Daniele Procida <daniele@vurt.org>
2025-03-19 07:29:29 +0100

    Added initial configuration

---
+++ conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+html_title = full_title = project = 'Mafanikio'
+copyright = '2025, Mafanikio contributors'
+author = 'Daniele Procida'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.intersphinx', 'sphinxcontrib.spelling']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'env']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_css_files = ["mafanikio.css"
+]
+
+intersphinx_mapping = {'vurt': ('https://vurt.eu', None)}
+
+spelling_lang = tokenizer_lang = "en_GB"
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Mafanikio
+=========
+
+Mafanikio is a handbook to help Africans achieve greater success in the global software industry.
+
